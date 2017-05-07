3e4fa9c93e2a5c252235cab8f5fb267eb4b6b3fd
tamzaselma <tamzaselma@gmail.com>
2017-05-07 11:39:17 +0200

    Update test.sql

--- BDD/test.sql
+++ BDD/test.sql
@@ -1,6 +1,6 @@
 set serverOutput on;
 Declare
-NumA Agent.agent#%Type := 100; --le numéro de l agent a tester est 100, donc normalement inexistant
+NumA Agent.agent_%Type := 100; --le numéro de l agent a tester est 100, donc normalement inexistant
 LA Agent%RowType; --liste des agents
 BEGIN
 LA := agent.getAgentById(NumA);
@@ -8,7 +8,23 @@ DBMS_OutPut.Put_Line('Nom de l agent = ' || LA.nom);
 DBMS_OutPut.Put_Line('Salaire de l agent = ' || LA.salaire);
 EXCEPTION
         WHEN NO_DATA_FOUND THEN
-                dbms_output.put_line('L agent n ' || NumP || ' n existe pas');
+                dbms_output.put_line('L agent n ' || NumA || ' n existe pas');
+                dbms_output.put_line('SQLCode =  ' || SQLCode);
+                dbms_output.put_line('SQLCode =  ' || sqlerrm);
+End;
+
+Declare
+NumA Agent.agent_%Type := 20;
+NomA Agent.nom%Type := 'ESLAN';
+TelA Agent.tel%Type := '0612222222';
+SalaireA Agent.salaire%Type := 1600;
+LA Agent%RowType; --liste des agents
+BEGIN
+LA := agent.agentinserer (NumA, NomA , TelA , SalaireA);
+DBMS_OutPut.Put_Line('L agent ' || LA.nom || ' a était inséré dans la table Agent');
+EXCEPTION
+        WHEN OTHER THEN
+                dbms_output.put_line('Erreur lors de l'insertion de l'agent numéro' || NumA);
                 dbms_output.put_line('SQLCode =  ' || SQLCode);
                 dbms_output.put_line('SQLCode =  ' || sqlerrm);
 End;
